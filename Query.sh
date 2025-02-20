7e9de0fbede40b91647a7e7f6c2ef3ff1442fdc1
Owen Halliday <hallidayo@moravian.edu>
2025-02-20 15:07:17 -0500

    add scripts for demo

---
+++ Query.sh
@@ -0,0 +1,7 @@
+echo ---SQL---
+echo Querying SQL
+python3 sql/Query.py "SELECT d.docket_title , c.comment, c.comment_id FROM dockets AS d JOIN comments AS c ON c.docket_id = d.docket_id;"
+
+echo ---OpenSearch---
+echo Querying OpenSearch
+python3 opensearch/query.py
